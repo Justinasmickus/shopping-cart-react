fbee1c4f6a52cf1f2b8dc4b67f0521599f9e1f91
Justinasmickus <mickusjustinas1@gmail.com>
2022-06-16 18:54:59 +0300

    CardButton component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 
+// icons
+
+
 // components
 import Item from './components/Item'
+import CartButton from "./components/CartButton";
+import { access } from "fs";
 
 //types
 export type CartItemType = {
@@ -27,7 +32,7 @@ const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
   console.log(data)
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -36,11 +41,19 @@ const App = () => {
 
 
   return (
-    <div className="grid p-6 justify-items-start  content-start sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 h-full gap-4">
-      {data?.map((item) => (
-        <Item key={item.id} item={item} handleAddToCart={handleAddToCart} />
-      ))}
+    <div>
+      <div className="">
+        <CartButton />
+      </div>
+      <div className="grid p-6 justify-items-start  content-start sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 h-full gap-4">
+
+
+        {data?.map((item) => (
+          <Item key={item.id} item={item} handleAddToCart={handleAddToCart} />
+        ))}
+      </div>
     </div>
+
   );
 }
 
